316d5611d0bf3df4cb3d9deb1375310513df0809
Susan Khatri <khatrisusan>
2020-02-11 10:38:50 +0100

    clear timeout

--- typewriter.js
+++ typewriter.js
@@ -6,17 +6,32 @@ function init(){
 let text = document.querySelector(".typewritten").textContent;
 let counter = 0;
 let letter = "";
- function typewriter(){
-    if (counter<=text.length){
+console.log(text.length);
+
+let timeId;
+function typewriter(){
+    const time = Math.random() * 250;
+    timeId = setTimeout(typewriter, time);
+    
+    if (counter<text.length){
         counter++;
     } 
-    letter = text.substr(0, counter)
+    else if(counter==text.length){
+        clearTime();
+        
+    }
+    letter = text.substr(0, counter);
     
-   
- document.querySelector(".typewritten").textContent = "";
- document.querySelector(".typewritten").textContent +=letter;
- const time = Math.random() * 250;
- setTimeout(typewriter, time)
- console.log(letter)
+    
+    document.querySelector(".typewritten").textContent = "";
+    document.querySelector(".typewritten").textContent +=letter;
+
+    console.log(letter)
+    
+    
+}
+function clearTime(){
+    clearTimeout(timeId);
+
 }
 
