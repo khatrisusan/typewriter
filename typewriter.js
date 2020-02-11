f643fc1e8e9f0e163eb88bf6b35a8be06e28b627
Susan Khatri <khatrisusan>
2020-02-11 02:48:04 +0100

    added typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,47 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", init)
+function init(){
+    typewriter();
+}
+let text = document.querySelector(".typewritten").textContent;
+let counter = 0;
+let letter = "";
+function typewriter(){
+    if (counter<=text.length){
+        counter++;
+    }
+    letter = text.substr(0, counter)
+   
+ document.querySelector(".typewritten").textContent = "";
+ document.querySelector(".typewritten").textContent +=letter;
+ setTimeout(typewriter, 400)
+ console.log(letter)
+}
+
+/* let counter;
+if (counter <= text.length){counter++}
+let character = text.substring(0,counter);
+window.addEventListener("DOMContentLoaded", init)
+function init(){
+    console.log("ready");
+           typeWriter();
+           //playSound();
+        //document.write(document.querySelector(.typewritten))
+}
+function typeWriter(){
+    console.log("blah");
+    document.querySelector(".typewritten").textContent += " ";
+    document.querySelector(".typewritten").textContent+= character;
+    setTimeout (typeWriter, 5000)
+}
+function playSound(){
+ 
+
+if (Math.random() > 0.5) {
+    document.querySelector("#typekey1").setAttribute("onplay");
+    } else {
+        document.querySelector("#typekey2").setAttribute("onplay");
+
+    }
+}
+ */
